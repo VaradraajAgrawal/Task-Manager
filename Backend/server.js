e344898be6f1378b75977db351182d4def7426e4
Varad <varadagrawal52@gmail.com>
2026-02-04 18:34:25 +0530

    Port Change

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,9 @@ app.use(express.json());
 
 app.use("/Task", Taskroute);
 
-const PORT = 5000;
-app.listen(PORT, () => {
+// ✅ Correct way
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
